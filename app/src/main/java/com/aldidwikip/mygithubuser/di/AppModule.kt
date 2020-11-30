84691a5bcd9003f0b93f3b661fd95a1b512b92ef
aldidwiki <aldidwiki@gmail.com>
2020-11-30 13:32:48 +0700

    optimize DI provider code

--- app/src/main/java/com/aldidwikip/mygithubuser/di/AppModule.kt
+++ app/src/main/java/com/aldidwikip/mygithubuser/di/AppModule.kt
@@ -3,9 +3,6 @@ package com.aldidwikip.mygithubuser.di
 import android.content.Context
 import android.content.SharedPreferences
 import com.aldidwikip.mygithubuser.R
-import com.aldidwikip.mygithubuser.data.AppRepository
-import com.aldidwikip.mygithubuser.data.local.LocalService
-import com.aldidwikip.mygithubuser.data.remote.RemoteService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,15 +13,6 @@ import javax.inject.Singleton
 @Module
 @InstallIn(ApplicationComponent::class)
 object AppModule {
-
-    @Singleton
-    @Provides
-    fun provideAppRepository(
-            remoteService: RemoteService,
-            localService: LocalService
-    ): AppRepository {
-        return AppRepository(remoteService, localService)
-    }
 
     @Singleton
     @Provides
